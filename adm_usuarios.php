211c2a4cd7cd7343f134ac6b0adf32eb02e66ea5
John Arisaca <jarisaca@gardilcic.cl>
2013-07-08 17:31:13 -0400

    Programas en desarrollo

--- adm_usuarios.php
+++ adm_usuarios.php
@@ -61,7 +61,7 @@
 
         <div class="container">
             
-            <legend>Bienvenido</legend>
+            <legend>Bienvenido al Sistema</legend>
             <br />
             
             <div id="menu_usuario">
